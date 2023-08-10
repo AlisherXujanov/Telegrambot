e9fc3e4c3515432bec672bd3d3c6da914a10eda9
Alisher <alisherxujanov163@gmail.com>
2023-08-10 23:03:01 +0500

    tg bot

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Telegrambot
+1. From telegram BotFather you will get a token that you need to get started with the bot. 
+2. Create a python file and write your token in it. 
+3. Install the python-telegram-bot library using pip install python-telegram-bot  
+   > pip install python-telegram-bot==13.7
+4. Create a bot instance using the token you got from the BotFather.
+5. Create a function that will be called whenever a user sends a message to the bot.
+   ex:  
+    def start(update, context):
+        context.bot.send_message("Hello World")
+
+6. Instantiate an updater object with the token you got from the BotFather and create a dispatcher object from it.
+7. Create a handler object for the command /start and pass the function start to it.
+   ex: 
+    dispatcher.add_handler(tg_ext.CommandHandler("start", start))
+8. Start polling the bot to check for new messages.
+    updater.start_polling()
+    updater.idle()
+9.  Run the python file and start chatting with your bot.
+
---
+++ main.py
@@ -0,0 +1,100 @@
+# pip install python-telegram-bot==13.7
+from requests import *
+from telegram import *
+from telegram.ext import *
+
+TOKEN = "6069762613:AAEf6C36OQl7j8-HqKmCt-FQdcyUT6P7l2s"
+
+RANDOM_IMAGE = "Random image"
+GET_MP3 = "Get mp3"
+RANDOM_IMG_URL = "https://picsum.photos/1200"
+
+# Declare IMAGE_COUNTER as a global variable
+global IMAGE_COUNTER
+IMAGE_COUNTER = 0
+
+
+print("Running up the bot...")
+
+# This command is used to register the commands
+# that the bot will be able to recognize and execute.
+# use_context=True is used to tell the bot to use the new context based callbacks
+# instead of the old deprecated ones.
+
+
+def start(update, context):
+    # SENDING HELLO MESSAGE
+    # .reply_text(message, reply_markup=None, **kwargs)
+    # update.message.reply_text("Hello there! I'm bot. Nice to see you!")
+    # ###################################################################
+    # SENDING PHOTO
+    _send_local_file(update, context)
+
+
+def _send_local_file(update, context):
+    """
+        We must open the file in binary mode, 
+        otherwise Telegram will not be able to process it correctly
+        ------------------------------------------------------------
+        RU: Мы должны открыть файл в двоичном режиме,
+        иначе Telegram не сможет обработать его правильно
+    """
+    with open("teleBot/me.jpg", "rb") as f:
+        """
+                update.message.reply_photo(photo, caption=None)
+            photo   - Photo to send
+            caption - Photo caption, 0-1024 characters
+        """
+        update.message.reply_photo(f, caption="Hello world! This is me!")
+
+
+def _send_mp3(update: Update, context: CallbackContext):
+    with open("teleBot/music.mp3", "rb") as f:
+        update.message.reply_audio(f, caption="This is mp3")
+
+
+def get_buttons(update: Update, context: CallbackContext):
+    buttons = [
+        [KeyboardButton(RANDOM_IMAGE)],
+        [KeyboardButton(GET_MP3)]
+    ]
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Choose option:",
+        reply_markup=ReplyKeyboardMarkup(buttons)
+    )
+
+
+def message_handler(update: Update, context: CallbackContext):
+    # Use the global keyword to modify IMAGE_COUNTER
+    global IMAGE_COUNTER
+    IMAGE_COUNTER += 1
+    if update.message.text == RANDOM_IMAGE:
+        image = get(RANDOM_IMG_URL).content
+        context.bot.sendMediaGroup(
+            chat_id=update.effective_chat.id,
+            media=[InputMediaPhoto(image, caption=f"Random {IMAGE_COUNTER}")]
+        )
+    elif update.message.text == GET_MP3:
+        _send_mp3(update, context)
+
+
+def help(update, context):
+    update.message.reply_text("""
+/start   - Start the bot
+/help    - Help
+/buttons - Get Optional buttons
+"""
+                              )
+
+
+updater = Updater(TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("help", help))
+dispatcher.add_handler(CommandHandler('buttons', get_buttons))
+dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
+
+
+updater.start_polling()
+updater.idle()
